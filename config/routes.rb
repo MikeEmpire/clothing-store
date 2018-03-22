18e24adcc8ebd2c79e4c3da5f83b95698d835906
Michael Olie <aolie94@gmail.com>
2018-03-22 09:30:17 -0700

    added sale price and edited navigation bar

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   mount Spree::Core::Engine, at: '/'
   Spree::Core::Engine.routes.draw do
     get "/sale" => "home#sale"
+    get "/about" => "home#about"
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
