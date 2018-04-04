1572710788b408cf7df45785a82a806ac6439521
Michael Olie <aolie94@gmail.com>
2018-04-03 18:35:20 -0700

    edited admin images

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   #
   # We ask that you don't use the :as option here, as Spree relies on it being
   # the default of "spree".
-  get "/.well-known/acme-challenge/KLofx7F_esunx3RxqMzvYZAAo8mB8Eu_W_suDvuDWs8" => "ssl#index"
   mount Spree::Core::Engine, at: '/'
   Spree::Core::Engine.routes.draw do
     root "home#home"
